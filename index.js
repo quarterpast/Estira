97db994ab7409e71039c8b18371ec54217a5da86
Matt Brennan <mattyb1000@gmail.com>
2013-11-22 16:43:31 +0000

    add umd

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 (function(){
-  var Estira;
-  module.exports = Estira = (function(){
-    Estira.displayName = 'Estira';
-    var prototype = Estira.prototype, constructor = Estira;
-    Estira.extend = function(proto, meta){
-      var super$;
-      super$ = this.prototype;
-      return (function(superclass){
-        var prototype = extend$(import$(constructor, superclass), superclass).prototype;
-        importAll$(prototype, arguments[1]);
-        function constructor(){
-          var this$ = this instanceof ctor$ ? this : new ctor$;
-          constructor.superclass.apply(this$, arguments);
-          this$.super$ = super$;
-          return this$;
-        } function ctor$(){} ctor$.prototype = prototype;
-        import$(constructor, meta);
-        return constructor;
-      }(this, proto));
-    };
-    function Estira(){
-      var this$ = this instanceof ctor$ ? this : new ctor$;
-      if (typeof this$.initialize === 'function') {
-        this$.initialize.apply(this$, arguments);
-      }
-      return this$;
-    } function ctor$(){} ctor$.prototype = prototype;
-    return Estira;
-  }());
+  (function(definition){
+    switch (false) {
+    case !(typeof define === 'function' && define.amd != null):
+      return define([], definition);
+    case typeof exports !== 'object':
+      return module.exports = definition();
+    default:
+      return this.Base = definition();
+    }
+  })(function(){
+    var Estira;
+    return Estira = (function(){
+      Estira.displayName = 'Estira';
+      var prototype = Estira.prototype, constructor = Estira;
+      Estira.extend = function(proto, meta){
+        var super$;
+        super$ = this.prototype;
+        return (function(superclass){
+          var prototype = extend$(import$(constructor, superclass), superclass).prototype;
+          importAll$(prototype, arguments[1]);
+          function constructor(){
+            var this$ = this instanceof ctor$ ? this : new ctor$;
+            constructor.superclass.apply(this$, arguments);
+            this$.super$ = super$;
+            return this$;
+          } function ctor$(){} ctor$.prototype = prototype;
+          import$(constructor, meta);
+          return constructor;
+        }(this, proto));
+      };
+      function Estira(){
+        var this$ = this instanceof ctor$ ? this : new ctor$;
+        if (typeof this$.initialize === 'function') {
+          this$.initialize.apply(this$, arguments);
+        }
+        return this$;
+      } function ctor$(){} ctor$.prototype = prototype;
+      return Estira;
+    }());
+  });
   function extend$(sub, sup){
     function fun(){} fun.prototype = (sub.superclass = sup).prototype;
     (sub.prototype = new fun).constructor = sub;
--- index.ls
+++ index.ls
@@ -1,4 +1,9 @@
-module.exports = class Estira
+<- (definition) ->
+	| typeof define is \function and define.amd? => define [] definition
+	| typeof exports is \object => module.exports = definition!
+	| otherwise => @Base = definition!
+
+return class Estira
 	@extend = (proto, meta)->
 		super$ = @::
 		class extends this implements proto

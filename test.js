51ac64ff4e65053fc0dce1b11f4e17f9de486d27
Matt Brennan <mattyb1000@gmail.com>
2014-01-31 15:33:57 +0000

    descriptive

--- test.js
+++ test.js
@@ -6,19 +6,19 @@ test('estira', function(t) {
 	var Foo = Base.extend();
 	var foo = new Foo();
 
-	t.ok(foo instanceof Foo);
-	t.ok(foo instanceof Base);
+	t.ok(foo instanceof Foo, 'instance of subclass');
+	t.ok(foo instanceof Base, 'instance of base class');
 
 	var Bar = Foo.extend();
 	var bar = new Bar();
 
-	t.ok(bar instanceof Bar);
-	t.ok(bar instanceof Foo);
-	t.ok(bar instanceof Base);
+	t.ok(bar instanceof Bar, 'instance of subsubclass');
+	t.ok(bar instanceof Foo, 'instance of subclass');
+	t.ok(bar instanceof Base, 'instance of base class');
 
 	var Baz = Base.extend(
 		function quux() {
-			t.ok(this instanceof Baz);
+			t.ok(this instanceof Baz, 'context is right class');
 			return 5;
 		}
 	).meta(
@@ -27,8 +27,8 @@ test('estira', function(t) {
 		}
 	);
 
-	t.ok(new Baz().quux() === 5);
-	t.ok(Baz.frob() === 6);
+	t.ok(new Baz().quux() === 5, 'methods work');
+	t.ok(Baz.frob() === 6, 'static methods work');
 
 	var Lorem = Baz.extend(
 		function ipsum() {
@@ -40,10 +40,10 @@ test('estira', function(t) {
 		}
 	);
 
-	t.ok(new Lorem().quux() === 5);
-	t.ok(new Lorem().ipsum() === 7);
-	t.ok(Lorem.frob() === 6);
-	t.ok(Lorem.dolor() === 8);
+	t.ok(new Lorem().quux() === 5, 'inherited methods work');
+	t.ok(new Lorem().ipsum() === 7, 'inherited methods work');
+	t.ok(Lorem.frob() === 6, 'inherited static methods work');
+	t.ok(Lorem.dolor() === 8, 'inherited static methods work');
 
 	var Sit = Baz.extend(
 		function quux() {
@@ -51,49 +51,49 @@ test('estira', function(t) {
 		}
 	);
 
-	t.ok(new Sit().quux() === 6);
+	t.ok(new Sit().quux() === 6, 'super$ calls work');
 
 	var Init = Base.extend(
 		function initialize(a) {
 			this.a = a;
-			t.ok(this instanceof Init);
-			t.ok(initialize.superclass$ === Base);
+			t.ok(this instanceof Init, 'right context');
+			t.ok(initialize.superclass$ === Base, 'superclass$ is set');
 		}
 	);
 
-	t.ok(new Init(5).a === 5);
+	t.ok(new Init(5).a === 5, 'initializer works');
 
 	var Sub = Init.extend(
 		function initialize(a) {
 			initialize.super$.call(this,a * 2);
-			t.ok(this instanceof Sub);
-			t.ok(initialize.superclass$ === Init);
+			t.ok(this instanceof Sub, 'right context');
+			t.ok(initialize.superclass$ === Init, 'superclass$ is set');
 		}
 	);
 
-	t.ok(new Sub(5).a === 10);
+	t.ok(new Sub(5).a === 10, 'inherited initializer works');
 
 	var SubSub = Sub.extend(
 		function initialize(a) {
 			initialize.super$.call(this,a * 2);
-			t.ok(initialize.superclass$ === Sub);
+			t.ok(initialize.superclass$ === Sub, 'superclass$ is set');
 		}
 	);
 
-	t.ok(new SubSub(5).a === 20);
+	t.ok(new SubSub(5).a === 20, 'double inherited initializer works');
 
 	function External(a) {
-		t.ok(this instanceof External);
+		t.ok(this instanceof External, 'right context');
 		this.a = a;
 	}
 
 	External.prototype.foo = function(b) {
-		t.ok(this instanceof External);
+		t.ok(this instanceof External, 'right context');
 		return this.a * b;
 	};
 
 	External.prototype.bar = function(b) {
-		t.ok(this instanceof External);
+		t.ok(this instanceof External, 'right context');
 		return this.a + b;
 	};
 
@@ -103,11 +103,11 @@ test('estira', function(t) {
 		}
 	);
 	var e = new ExternalSub(5);
-	t.ok(e instanceof ExternalSub);
-	t.ok(e instanceof External);
-	t.ok(e.a === 5);
-	t.ok(e.bar(4) === 9);
-	t.ok(e.foo(4) === 30);
+	t.ok(e instanceof ExternalSub, 'instance of right class');
+	t.ok(e instanceof External, 'instance of right class');
+	t.ok(e.a === 5, 'initializer works');
+	t.ok(e.bar(4) === 9, 'inherited method work');
+	t.ok(e.foo(4) === 30, 'inherited method work');
 
 	var ExternalSubSub = ExternalSub.extend(
 		function initialize(a) {
@@ -115,10 +115,10 @@ test('estira', function(t) {
 		}
 	);
 	var es = new ExternalSubSub(5);
-	t.ok(es.a === 10);
-	t.ok(es instanceof ExternalSubSub);
-	t.ok(es instanceof ExternalSub);
-	t.ok(es instanceof External);
+	t.ok(es.a === 10, 'initializer works');
+	t.ok(es instanceof ExternalSubSub, 'instance of right class');
+	t.ok(es instanceof ExternalSub, 'instance of right class');
+	t.ok(es instanceof External, 'instance of right class');
 
 	t.end();
 });

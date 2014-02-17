99d8fc01a4b360616b98488390bcfd099ffe5ab4
Matt Brennan <mattyb1000@gmail.com>
2014-02-17 12:07:54 +0000

    update syntax

--- test.js
+++ test.js
@@ -23,71 +23,71 @@ test('double extend', function(t) {
 	t.end();
 });
 test('methods', function(t) {
-	var Baz = Base.extend(
-		function quux() {
+	var Baz = Base.extend({
+		quux: function() {
 			t.ok(this instanceof Baz, 'context is right class');
 			return 5;
 		}
-	).meta(
-		function frob() {
+	}).meta({
+		frob: function() {
 			return 6;
 		}
-	);
+	});
 
 	t.ok(new Baz().quux() === 5, 'methods work');
 	t.ok(Baz.frob() === 6, 'static methods work');
 
-	var Lorem = Baz.extend(
-		function ipsum() {
+	var Lorem = Baz.extend({
+		ipsum: function() {
 			return 7;
 		}
-	).meta(
-		function dolor() {
+	}).meta({
+		dolor: function() {
 			return 8;
 		}
-	);
+	});
 
 	t.ok(new Lorem().quux() === 5, 'inherited methods work');
 	t.ok(new Lorem().ipsum() === 7, 'inherited methods work');
 	t.ok(Lorem.frob() === 6, 'inherited static methods work');
 	t.ok(Lorem.dolor() === 8, 'inherited static methods work');
 
-	var Sit = Baz.extend(
-		function quux() {
+	var Sit = Baz.extend({
+		quux: function quux() {
 			return quux.super$.apply(this, arguments) + 1;
 		}
-	);
+	});
 
 	t.ok(new Sit().quux() === 6, 'super$ calls work');
 
 	t.end();
 });
 test('initializers', function(t) {
-	var Init = Base.extend(
-		function initialize(a) {
+	var Init = Base.extend({
+		initialize: function(a) {
 			this.a = a;
 			t.ok(this instanceof Init, 'right context');
 			t.ok(initialize.superclass$ === Base, 'superclass$ is set');
 		}
-	);
+	});
 	t.ok(new Init(5).a === 5, 'initializer works');
 
-	var Sub = Init.extend(
-		function initialize(a) {
+	var Sub = Init.extend({
+		initialize: function initialize(a) {
 			initialize.super$.call(this,a * 2);
 			t.ok(this instanceof Sub, 'right context');
 			t.ok(initialize.superclass$ === Init, 'superclass$ is set');
 		}
-	);
+	});
 
 	t.ok(new Sub(5).a === 10, 'inherited initializer works');
 
-	var SubSub = Sub.extend(
-		function initialize(a) {
+	var SubSub = Sub.extend({
+		initialize: function initialize(a) {
 			initialize.super$.call(this,a * 2);
 			t.ok(initialize.superclass$ === Sub, 'superclass$ is set');
 		}
-	);
+	});
 
 	t.ok(new SubSub(5).a === 20, 'double inherited initializer works');
 
@@ -121,11 +121,11 @@ test('external classes', function(t) {
 	t.ok(e.bar(4) === 9, 'inherited method work');
 	t.ok(e.foo(4) === 30, 'inherited method work');
 
-	var ExternalSubSub = ExternalSub.extend(
-		function initialize(a) {
+	var ExternalSubSub = ExternalSub.extend({
+		initialize: function initialize(a) {
 			initialize.super$(a * 2);
 		}
-	);
+	});
 	var es = new ExternalSubSub(5);
 	t.ok(es.a === 10, 'initializer works');
 	t.ok(es instanceof ExternalSubSub, 'instance of right class');
@@ -136,11 +136,11 @@ test('external classes', function(t) {
 });
 
 test('bare constructors', function(t) {
-	var Bare = Base.extend(
-		function initialize(a) {
+	var Bare = Base.extend({
+		initialize: function(a) {
 			this.a = a;
 		}
-	);
+	});
 
 	var b = Bare(5);
 	t.ok(b instanceof Bare, 'bare constructors work');
